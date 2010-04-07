6df51d72dc30a5c4dbaccb6343e651ef3126cab7
Martin Elff <melff@essex.ac.uk>
2010-04-07 00:00:00 +0000

    version 0.2-1

---
+++ R/zzz.R
@@ -0,0 +1,33 @@
+.First.lib <- .onLoad <- function(lib,pkg){
+  if(require(memisc)){
+    setSummaryTemplate(
+      mclogit = c(
+              #"McFadden R-sq." = "($McFadden:f#)",
+              #"Cox-Snell R-sq." = "($Cox.Snell:f#)",
+              #"Nagelkerke R-sq."  = "($Nagelkerke:f#)",
+              Dispersion         = "($phi:#)",
+              "Likelihood-ratio" = "($LR:f1#)",
+              #p             = "($p:#)",
+              #"Log-likelihood" = "($logLik:f1#)",
+              Deviance      = "($deviance:f1#)",
+              #AIC           = "($AIC:f1#)",
+              #BIC           = "($BIC:f1#)",
+              N             = "($N:d)"
+      ),
+      mclogitRandeff = c(
+              #"McFadden R-sq." = "($McFadden:f#)",
+              #"Cox-Snell R-sq." = "($Cox.Snell:f#)",
+              #"Nagelkerke R-sq."  = "($Nagelkerke:f#)",
+              Dispersion         = "($phi:#)",
+              #"Likelihood-ratio" = "($LR:f1#)",
+              #p             = "($p:#)",
+              #"Log-likelihood" = "($logLik:f1#)",
+              Deviance      = "($deviance:f1#)",
+              #AIC           = "($AIC:f1#)",
+              #BIC           = "($BIC:f1#)",
+              N             = "($N:d)"
+      )
+    )
+  }
+}
+
